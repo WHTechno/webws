87947df167f597084191d556c0086eeaa214293d
WHTech <93919489+WHTechno@users.noreply.github.com>
2025-06-11 07:06:50 +0700

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -27,7 +27,7 @@
     </div>
 
     <!-- Team Members Grid -->
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
       <div
         v-for="member in team"
         :key="member.name"
